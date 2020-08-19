9584c61c96de9e824d6c72b545fc3cb9d3c720f5
takushyo <50485193+takushyo@users.noreply.github.com>
2020-08-19 10:13:20 +1000

    Add project files.

---
+++ TakuLib/Classes/StateMachine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEditor;
+namespace TakuLib.StateMachine {
+	public class StateMachine<T> {
+		
+		public State<T> currentState { get; private set; }
+		public T Owner;
+		public StateMachine(T _owner) {
+			Owner = _owner;
+			
+			currentState = null;
+		}
+
+		public void ChangeState(State<T> _newState) {
+			if (currentState != null)
+				currentState.ExitState(Owner);
+			currentState = _newState;
+			currentState.EnterState(Owner);
+		}
+
+		public void Update() {
+			if (currentState != null)
+				currentState.UpdateState(Owner);
+		}
+
+		public void FixedUpdate() {
+			if (currentState != null)
+				currentState.FixedUpdate(Owner);
+		}
+	}
+
+	[Serializable]
+	public abstract class State<T>
+	{
+		
+		protected string stateName;
+		public virtual State<T> createInstance() { Debug.WriteLine("diddnt work"); return null; }
+		public virtual string GetFlavourText() { return "STATE FLAVOUR NOT SET!"; }
+		public virtual string GetStateName() => this.GetType().Name;
+		public abstract void EnterState(T _owner);
+		public abstract void ExitState(T _owner);
+		public abstract void UpdateState(T _owner);
+		public abstract void FixedUpdate(T _owner);
+	}
+}
