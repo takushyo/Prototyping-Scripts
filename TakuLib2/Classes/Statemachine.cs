8697e00b5fb5c5a45762a09db2d90ef42e35cc01
takushyo <50485193+takushyo@users.noreply.github.com>
2020-08-19 11:14:07 +1000

    Vector2 extensions added. Componant getters added
    who even likes typing getcomponent every time they want to get recttransform :cry:

---
+++ TakuLib2/Classes/Statemachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TakuLib.StateMachine {
+	public class StateMachine<T> {
+
+		public State<T> currentState { get; private set; }
+		public T Owner;
+		public StateMachine(T _owner) {
+			Owner = _owner;
+
+			currentState = null;
+		}
+
+		public void ChangeState(State<T> _newState) {
+			if (currentState != null)
+				currentState.ExitState(Owner);
+			currentState = _newState;
+			currentState.EnterState(Owner);
+		}
+
+		public void Update() {
+			if (currentState != null)
+				currentState.UpdateState(Owner);
+		}
+
+		public void FixedUpdate() {
+			if (currentState != null)
+				currentState.FixedUpdate(Owner);
+		}
+	}
+
+	[Serializable]
+	public abstract class State<T> {
+		protected string stateName;
+		public virtual State<T> createInstance() { return null; }
+		public virtual string GetStateName() => this.GetType().Name;
+		public abstract void EnterState(T _owner);
+		public abstract void ExitState(T _owner);
+		public abstract void UpdateState(T _owner);
+		public abstract void FixedUpdate(T _owner);
+	}
+}
---
+++ TakuLib2/Classes/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace TakuLib {
+
+    class Util { }
+
+	public static class VectorExtensions {
+        public static Vector3 With(this Vector3 vec, float? x = null, float? y = null, float? z = null, bool additive = false) =>
+        additive == false ? new Vector3(x ?? vec.x, y ?? vec.y, z ?? vec.z) :
+                            new Vector3(x + vec.x ?? vec.x, y + vec.y ?? vec.y, z + vec.z ?? vec.z);
+        public static Vector2 With(this Vector2 vec, float? x = null, float? y = null, bool additive = false) =>
+            additive == false ? new Vector2(x ?? vec.x, y ?? vec.y) :
+                                new Vector2(x + vec.x ?? vec.x, y + vec.y ?? vec.y);
+    }
+
+    public static class ComponantExtensions {
+        public static RectTransform GetRect(this GameObject obj) => obj.GetComponent<RectTransform>();
+        public static RectTransform GetRect(this Transform obj) => obj.GetComponent<RectTransform>();
+    }
+}
