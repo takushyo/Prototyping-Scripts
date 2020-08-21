1814a4d1ae71ef9e4eb6bfda5d96beb0c99b4d40
takushyo <50485193+takushyo@users.noreply.github.com>
2020-08-21 15:24:37 +1000

    FloatRange and comments

--- TakuLib/Classes/Util.cs
+++ TakuLib/Classes/Util.cs
@@ -8,9 +8,35 @@ using UnityEngine;
 
 namespace TakuLib {
 
-    class Util { }
+    class Util {
+        /// <summary>
+        /// Will give a 0-1 value from a range,
+        /// 
+        /// USAGE: 
+        /// 
+        /// float k = 5;
+        /// NormalizeFloatRange(ref k, 0, 10);
+        /// k will equal 0.5f
+        /// 
+        /// float k = 5;
+        /// NormalizeFloatRange(ref k, 5, 10);
+        /// k will equal 0.0f
+        /// </summary>
+        public static void NormalizeFloatRange(ref float Current, float min, float max) => Current = (Current - min) / (max - min);
+    }
 
     public static class VectorExtensions {
+        /// <summary>
+        /// USAGE: transform.position = transform.position.With(x: 1);
+        /// this will effectivly do the same as
+        /// new Vector3(1, transform.position.y, transform.position.z);
+        /// 
+        /// additive bool will add it onto existing field
+        /// transform.position = transform.position.With(y: 5, additive: true);
+        /// will be the same as
+        /// new Vector3(transform.position.x, transform.position.y + 5, tranform.position.z);
+        /// </summary>
+        
         public static Vector3 With(this Vector3 vec, float? x = null, float? y = null, float? z = null, bool additive = false) =>
         additive == false ? new Vector3(x ?? vec.x, y ?? vec.y, z ?? vec.z) :
                             new Vector3(x + vec.x ?? vec.x, y + vec.y ?? vec.y, z + vec.z ?? vec.z);
@@ -20,13 +46,30 @@ namespace TakuLib {
     }
 
     public static class ComponantExtensions {
+        /// <summary>
+        /// USAGE: 
+        /// RectTransform newRect = gameObject.GetRect();
+        /// RectTransform newRect = gameObject.Transform.GetRect();
+        /// </summary>
         public static RectTransform GetRect(this GameObject obj) => obj.GetComponent<RectTransform>();
         public static RectTransform GetRect(this Transform obj) => obj.GetComponent<RectTransform>();
     }
 
     public static class SubclassEnumerator {
-        static SubclassEnumerator() { }
+        /// <summary>
+        /// This will return an Ienumerable of your subtypes of a class that isnt abstract
+        /// EG
+        /// public absract Item
+        /// public Eggs : Item
+        /// public Tomatos : Item
+        /// 
+        /// In this instance, Eggs and Items will be returned in an Ienumerable<Item>
+        /// 
+        /// USAGE
+        /// List<Item> ItemTypes = GetEnumerableOfSubTypes<Item>(null) as Item;
+        /// </summary>
 
+        
         public static IEnumerable<T> GetEnumerableOfSubTypes<T>(params object[] args) where T : class {
             List<T> objects = new List<T>();
             foreach (Type type in
@@ -36,6 +79,18 @@ namespace TakuLib {
             }
             return objects;
         }
+
+        /// <summary>
+        /// This is pretty much the same as above but forced into a dictionary. you will have to cast the result from type in the dictionary
+        /// as you cannot have a generic entry for a value.
+        /// 
+        /// This is REALLY helpful to build a database of items and have a string reference to them.
+        /// using the string reference you can save via JSON just the type name and look it back up on load
+        /// 
+        /// USAGE:
+        /// Dictionary<string, Type> ItemTypes = GetDictOfSubTypes<Item>(null);
+        /// Item item = ItemTypes["Tomato"] as Item;
+        /// </summary>
 
         public static Dictionary<string, Type> GetDictOfSubTypes<T>(params object[] args) where T : class {
             var dict = new Dictionary<string, Type>();
